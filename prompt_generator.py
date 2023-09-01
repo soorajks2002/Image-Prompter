2e47531798c12484deacf0662133b6681725ec1a
sooraj <soorajks2002@gmail.com>
2023-09-01 14:26:39 +0530

    function to generate prompts

---
+++ prompt_generator.py
@@ -0,0 +1,19 @@
+import openai
+from api_key import open_ai_api_key
+
+openai.api_key = open_ai_api_key
+
+def generate_prompt (keywords) :
+    
+    prompt = "write an image prompt for DALL-E using these words {keywords}"
+
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": "write an image prompt for DALL-E using these words [human, bike, bottle]"},
+                {"role": "assistant", "content": "Generate an image of a human riding a bicycle while holding a water bottle."},
+                {"role": "user", "content": prompt.format(keywords=keywords)}
+            ]
+        )
+
+    return response
