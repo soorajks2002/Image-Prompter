62fbc57c7c842a0edd9c9ecac94147823878cad0
sooraj <soorajks2002@gmail.com>
2023-09-02 18:43:30 +0530

    updated streamlit UI

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 from streamlit_tags import st_tags
 from prompt_generator import generate_prompt
+from PIL import Image
 
-st.set_page_config(page_title="Image Prompt Generator")
+favicon = Image.open("favicon.ico")
+st.set_page_config(page_title="Image Prompt Generator", page_icon=favicon)
 
 st.title("Image Prompt Generator")
+st.markdown("##")
 
-keywords = st_tags(
-    label='#### Enter Keywords:',
-    text='Press enter to add more',
-    maxtags=5)
+with st.form("forms") :
 
-if st.button("SUBMIT") and keywords :
-    with st.spinner('Generating...'):
-        prompt = generate_prompt(keywords)
-    st.header("Prompt :")
-    st.markdown(f"#### {prompt}")
+    keywords = st_tags(
+        label='#### Enter Keywords:',
+        text='Press enter to add more (max 5 keywords)',
+        maxtags=5)
+
+    if st.form_submit_button("Generate Prompt") and keywords :
+        with st.spinner('Generating...'):
+            for i in range(2) :
+                st.divider()
+                prompt = generate_prompt(keywords)
+                st.markdown(f"## Prompt {i+1} :")
+                st.markdown(f"#### *{prompt}*")
