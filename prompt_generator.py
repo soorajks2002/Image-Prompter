a35ae3f79840839d640aa46cecc7e58df34c79d5
sooraj <soorajks2002@gmail.com>
2023-09-07 19:42:10 +0530

    updated prompt format for better results

--- prompt_generator.py
+++ prompt_generator.py
@@ -15,14 +15,16 @@ def extract_sentences(input_text):
 
 def generate_prompt (nprompt, style, keywords) :
     
-    prompt = '''write {nprompt} short and simple {style} like image prompt for a diffusion based image generation model using these words {keywords}'''
+    prompt = '''write {nprompt} short, simple and {style} art like image prompt for a diffusion based image generation model using these words {keywords}'''
 
     response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "user", "content": '''write 3 short and simple anime like image prompt for a diffusion based image generation model using these words ["kids", "healthy food", "cereal"]'''},
-                {"role": "assistant", "content": '''An anime scene featuring joyful kids gathered around a vibrant picnic blanket loaded with colorful, healthy food options like fruits and vegetables, with a cereal box as the centerpiece.\n An adorable anime character, dressed in a chef's outfit, preparing a delicious and nutritious breakfast using cereal as a key ingredient while teaching a group of kids about the importance of balanced meals.\n A heartwarming anime moment where children in a daycare setting engage in a fun, educational activity involving planting and growing their own cereal crops, symbolizing the connection between healthy food and growth.'''},
-                {"role": "user", "content": prompt.format(nprompt=nprompt, keywords=keywords, style=style)}
+            {"role": "user","content": '''write 3 short and simple digital art like image prompt for a diffusion based image generation model using these words ["kids", "healthy food", "cereal"]'''},
+            {"role": "assistant", "content": '''Create a digital artwork that portrays kids in a colorful, abstract world made entirely of vibrant, oversized cereal pieces and nutritious ingredients.\n Craft a digital painting of children happily engaged in a friendly cereal eating competition, showcasing their enjoyment of wholesome, nutritious breakfast choices.\n Design a digital art scene where kids embark on a magical adventure inside a cereal box, discovering a miniature universe filled with talking cereal characters and fantastical landscapes made of healthy food.'''},
+            {"role": "user","content": '''write 2 short and simple anime art like image prompt for a diffusion based image generation model using these words ["ramen shop", "humans", "enjoying food"]'''},
+            {"role": "assistant", "content": '''An anime scene depicting a bustling ramen shop filled with humans savoring steaming bowls of ramen, their faces lit up with joy as they enjoy every slurp of the delicious noodles.\n Create an anime-style illustration of a group of friends sitting at a cozy ramen shop, bonding over their shared love for food, with warm smiles and animated expressions as they savor the flavors.'''},
+            {"role": "user", "content": prompt.format(nprompt=nprompt, keywords=keywords, style=style)}
             ]
         )
 
