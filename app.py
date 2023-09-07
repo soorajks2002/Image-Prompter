c6cb8e5713b040becead73d94589a94ff9a17b51
sooraj <soorajks2002@gmail.com>
2023-09-07 08:12:28 +0530

    Updated UI and Changed Prompt for 1. option to select number of prompts to be generated 2. select art style like releastic, comic, anime, etc

--- app.py
+++ app.py
@@ -9,17 +9,31 @@ st.set_page_config(page_title="Image Prompt Generator", page_icon=favicon)
 st.title("Image Prompt Generator")
 st.markdown("##")
 
-with st.form("forms") :
+input_form = st.form("forms")
+output_container = st.container()
 
+def display_prompt() :
+    with st.spinner('Generating...'):
+        prompts = generate_prompt(nprompt, style, keywords)
+        if len(prompts) != nprompt  :
+            display_prompt() 
+        for i,prompt in enumerate(prompts):
+            output_container.divider()
+            output_container.markdown(f"### Prompt {i+1} :")
+            output_container.markdown(f"##### *{prompt}*")
+
+with input_form:
     keywords = st_tags(
-        label='#### Enter Keywords:',
+        label='#### Enter Keywords ...',
         text='Press enter to add more (max 5 keywords)',
         maxtags=5)
 
-    if st.form_submit_button("Generate Prompt") and keywords :
-        with st.spinner('Generating...'):
-            for i in range(2) :
-                st.divider()
-                prompt = generate_prompt(keywords)
-                st.markdown(f"## Prompt {i+1} :")
-                st.markdown(f"#### *{prompt}*")
+    c1, c2 = st.columns(2)
+    nprompt = c1.select_slider("Select Number of Prompts ...", options=[1, 2, 3, 4, 5])
+    style = c2.selectbox("Select Art Style ...", options=["Realistic", "Anime", "Watercolor", "Academicism painting", "John Collier", "Salvador Dali", "Android Jones"])
+
+    if st.form_submit_button("Generate Prompt"):
+        if keywords :
+            display_prompt()
+        else :
+            st.warning("Pleae select keywords ...")
--- prompt_generator.py
+++ prompt_generator.py
@@ -1,19 +1,31 @@
 import openai
+import re
 from api_key import open_ai_api_key
 
 openai.api_key = open_ai_api_key
 
-def generate_prompt (keywords) :
+def extract_sentences(input_text):
+    # Split the input text into sentences based on both "\n\n" and "\n"
+    sentences = re.split(r'(\n\n|\n)', input_text)
     
-    prompt = "write an image prompt for DALL-E using these words {keywords}"
+    # Remove leading numbers and periods from sentences
+    sentences = [re.sub(r'^\d+\.\s*', '', sentence.strip()) for sentence in sentences if sentence.strip()]
+    
+    return sentences
+
+def generate_prompt (nprompt, style, keywords) :
+    
+    prompt = '''write {nprompt} short and simple {style} like image prompt for a diffusion based image generation model using these words {keywords}'''
 
     response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "user", "content": "write an image prompt for DALL-E using these words [human, bike, bottle]"},
-                {"role": "assistant", "content": "Generate an image of a human riding a bicycle while holding a water bottle."},
-                {"role": "user", "content": prompt.format(keywords=keywords)}
+                {"role": "user", "content": '''write 3 short and simple anime like image prompt for a diffusion based image generation model using these words ["kids", "healthy food", "cereal"]'''},
+                {"role": "assistant", "content": '''An anime scene featuring joyful kids gathered around a vibrant picnic blanket loaded with colorful, healthy food options like fruits and vegetables, with a cereal box as the centerpiece.\n An adorable anime character, dressed in a chef's outfit, preparing a delicious and nutritious breakfast using cereal as a key ingredient while teaching a group of kids about the importance of balanced meals.\n A heartwarming anime moment where children in a daycare setting engage in a fun, educational activity involving planting and growing their own cereal crops, symbolizing the connection between healthy food and growth.'''},
+                {"role": "user", "content": prompt.format(nprompt=nprompt, keywords=keywords, style=style)}
             ]
         )
 
-    return response["choices"][0]["message"]["content"]
+    generated_sentences = response["choices"][0]["message"]["content"]
+    
+    return extract_sentences(generated_sentences)
